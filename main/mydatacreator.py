88124ef223e289bfabc108564357dd7a3323c4db
DavidLaszlo <david.laci20@gmail.com>
2015-12-09 01:02:02 +0200

    refined implementation, added progress

--- main/mydatacreator.py
+++ main/mydatacreator.py
@@ -1,11 +1,23 @@
 import os
 import re
 
+myglobvar = 0
+previous = 0
 
-def create_data(search_path, train_keys):
+
+def create_data(search_path, train_keys, num_tasks):
+    global myglobvar
+    global previous
+    myglobvar += 1
+    actual = int(myglobvar/num_tasks*100)
+    if actual > previous:
+        print(actual)
+        previous = actual
     values = {}
+    if not os.path.exists(search_path):
+        return values
     filename = os.path.basename(search_path)
-    with open(search_path, 'rb') as infile:
+    with open(search_path, 'r') as infile:
         text = infile.read()
     values['file'] = filename
     if filename in train_keys:
--- main/mymain.py
+++ main/mymain.py
@@ -4,30 +4,43 @@ Truly Native?
 __author__ : David Shinn
 """
 from __future__ import print_function
-import datacreator
+import mydatacreator
 import myrandomforestclassifier
 
 import glob
 import multiprocessing
-
+import time
+import datetime
 import pandas as pd
 
+
+def print_time():
+    ts = time.time();
+    st = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+    print(st)
+
+print('Hello')
+print_time()
 print('--- Read training labels')
 train_labels = pd.read_csv('../../../data/train_v2.csv')
 train_keys = dict([a[1] for a in train_labels.iterrows()])
-test_files = set(pd.read_csv('../../../data/sampleSubmission_v2.csv').file.values)
 
+num_tasks = len(train_labels)
+print('Train labels found: ')
+print(num_tasks)
 file_paths = glob.glob('../../../data/*/*.txt')
-num_tasks = len(file_paths)
 results = {}
 if __name__ == '__main__':
     p = multiprocessing.Pool()
-    results = p.map([datacreator.create_data(path, train_keys) for path in file_paths])
+    results = p.map([mydatacreator.create_data(path, train_keys, num_tasks) for path in file_paths])
     p.close()
     p.join()
 
 print("--- Data read and mapped: {:,}".format(len(results), end=''))
 df_full = pd.DataFrame(list(results))
 
+
+test_files = set(pd.read_csv('../../../data/sampleSubmission_v2.csv').file.values)
 submission = myrandomforestclassifier.classify(pd.DataFrame(list(results)), test_files)
 submission.to_csv('../../../native_btb_basic_submission.csv', index=False)
+print_time()
